42859ac131cbbc95deec288d2780f1ae1cb421e4
Aaron V <aaron@buildsomethingawesome.org>
2014-06-08 23:30:48 -0700

    Added REST API to play the mp3

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 var child_process = require('child_process');
 
-console.log('Running!');
-child_process.exec('afplay boogie.mp3', function() {
-	console.log('Done playing!');
-})
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 8080;
+
+var router = express.Router();
+
+router.post('/play', function() {
+  child_process.exec('afplay boogie.mp3', function() {
+    console.log('Done playing!');
+  });
+});
+
+app.use('/api', router);
+
+app.listen(port);
+console.log('Magic happens on port ' + port);
