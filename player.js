00272891d581877424ccb7b59a31219195eef163
Aaron V <aaron@buildsomethingawesome.org>
2014-06-10 07:06:23 -0700

    Stop any currently-playing songs when starting a new one

--- player.js
+++ player.js
@@ -1,6 +1,13 @@
 var child_process = require('child_process');
 
+var currentPlayProcess = undefined;
+
 exports.play = function(filename) {
+  if (currentPlayProcess) {
+    console.log('Stopping current song');
+    currentPlayProcess.kill('SIGTERM');
+  }
+  
   console.log('Playing ' + filename);
-  child_process.spawn('afplay', [filename]);
+  currentPlayProcess = child_process.spawn('afplay', [filename]);
 }
