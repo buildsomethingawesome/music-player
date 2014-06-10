af4f9811da768baeb27298ef15f2175c9a6a5cd8
Aaron V <aaron@buildsomethingawesome.org>
2014-06-09 21:27:48 -0700

    UI mockup for the list of songs

--- frontend/main.js
+++ frontend/main.js
@@ -8,7 +8,22 @@ var MusicPlayerApp = React.createClass({
     httpinvoke('/api/play', 'POST');
   },
   render: function() {
-    return <button className="btn btn-primary" onClick={this.play}>Play</button>;
+    var songs = [
+    { artist: 'Carl Sonny Leyland Trio', title: 'This Is The Blues' },
+    { artist: 'Carl Sonny Leyland Trio', title: 'Rat Catcher\'s Blues' },
+    { artist: 'Carsie Blanton', title: 'Azelea' },
+    { artist: 'Josh Fialkoff', title: 'Is You Is or Is You Ain\'t My Baby' },
+    ];
+    
+    var listItems = [];
+    songs.forEach(function(song) {
+      listItems.push(<li className="list-group-item"><b>{song.title}</b> - {song.artist}</li>);
+    });
+    
+    return <div>
+      <button className="btn btn-primary" onClick={this.play}>Play</button>
+      <ul className="list-group">{listItems}</ul>
+    </div>;
   }
 })
 
