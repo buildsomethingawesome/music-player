c352403a25b8c14b389d892a84ebad617b5850e3
Aaron V <aaron@buildsomethingawesome.org>
2014-06-09 23:26:39 -0700

    Fix bf60f7: Extract api.js and songs.js

--- api.js
+++ api.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var songs = require('./songs');
+var child_process = require('child_process');
 
 var api = express.Router();
 
--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-var child_process = require('child_process');
 var express = require('express');
 
 var app = express();
